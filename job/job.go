48f1659291486042cd59ca2a4fcb4fc7e99f3002
Arcadio Carballares <arcadio.carballares@viewnext.com>
2022-01-03 14:32:26 +0100

    FirstJob code engine. Test 1.

--- job/job.go
+++ job/job.go
@@ -6,5 +6,5 @@ import (
 )
 
 func main() {
-	fmt.Printf("Hi from a batch job! My index is: %s\n", os.Getenv("JOB_INDEX"))
+	fmt.Printf("Hi, Arcadio 1, from a batch job! My index is: %s\n", os.Getenv("JOB_INDEX"))
 }
